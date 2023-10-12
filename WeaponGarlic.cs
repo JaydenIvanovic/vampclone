f096ced5047ba326307979f2fa3acd8f3fa66142
Jayden Ivanovic <jayden.ivanovic@gmail.com>
2023-10-12 22:28:39 +1100

    Fix garlic not triggering damage

--- WeaponGarlic.cs
+++ WeaponGarlic.cs
@@ -1,43 +1,51 @@
+using System.Linq;
 using Constants;
 using Godot;
 
 public partial class WeaponGarlic : Area2D {
 	private CollisionShape2D collisionShape2D;
-	private float radius = 0f;
+	private Area2D[] enemiesInRadius;
+	private Timer damageTicker;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
 		collisionShape2D = GetNode<CollisionShape2D>("./CollisionShape2D");
-		(collisionShape2D.Shape as CircleShape2D).Radius = radius;
+		(collisionShape2D.Shape as CircleShape2D).Radius = 0f;
 
-		// TODO: Need to check when enemies exit the area as well.
-		// That is, the logic needs to be something like...
-		// On enter, set a variable that this enemy should take tick
-		// damage from Garlic periodically.
-		// On exit, remove this variable.
-		AreaEntered += HandleCollision;
+		enemiesInRadius = new Area2D[] { };
 
-		Events.getInstance().ExperienceGemAcquired += HandleExperienceAcquired;
-	}
+		damageTicker = new Timer {
+			OneShot = false,
+			Autostart = true,
+			WaitTime = 0.5
+		};
+		damageTicker.Timeout += () => {
+			foreach (var enemy in enemiesInRadius) {
+				(enemy as IEnemy).TakeDamage(15.0f);
+			}
+		};
+		AddChild(damageTicker);
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta) {
+		AreaEntered += (Area2D target) => {
+			if (target.IsInGroup(Groups.ENEMIES)) {
+				enemiesInRadius = enemiesInRadius.Append(target).ToArray();
+			}
+		};
+		AreaExited += (Area2D target) => {
+			if (target.IsInGroup(Groups.ENEMIES)) {
+				enemiesInRadius = enemiesInRadius.Where(e => e.GetInstanceId() != target.GetInstanceId()).ToArray();
+			}
+		};
+
+		Events.getInstance().ExperienceGemAcquired += () => {
+			var garlicLevels = Globals.GameState.experienceGained / 100;
+			// TODO: Make this logarithmic?
+			(collisionShape2D.Shape as CircleShape2D).Radius = 13 * garlicLevels;
+			QueueRedraw();
+		};
 	}
 
 	public override void _Draw() {
-		DrawCircle(Vector2.Zero, radius, Colors.LightGreen);
-	}
-
-	private void HandleCollision(Area2D target) {
-		if (target.IsInGroup(Groups.ENEMIES)) {
-			(target as IEnemy).TakeDamage(10.0f);
-		}
-	}
-
-	private void HandleExperienceAcquired() {
-		var garlicLevels = Globals.GameState.experienceGained / 100;
-		// TODO: Make this logarithmic?
-		radius = 13 * garlicLevels;
-		QueueRedraw();
+		DrawCircle(Vector2.Zero, (collisionShape2D.Shape as CircleShape2D).Radius, Colors.LightGreen);
 	}
 }
