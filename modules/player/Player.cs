9dcf080446a70353315f8a90617962d121ce2a66
Jayden Ivanovic <jayden.ivanovic@gmail.com>
2024-05-04 15:32:38 +1000

    Rename boundary checking ray cast

--- modules/player/Player.cs
+++ modules/player/Player.cs
@@ -3,12 +3,12 @@ using Godot;
 public partial class Player : Area2D, IMover {
     private float speed = 30;
     private AnimatedSprite2D sprite;
-    private RayCast2D rayCast;
+    private RayCast2D boundaryMaskedRay;
 
     public Vector2 Direction { get; private set; } = Vector2.Zero;
 
     public override void _Ready() {
-        rayCast = GetNode<RayCast2D>("./RayCast2D");
+        boundaryMaskedRay = GetNode<RayCast2D>("./BoundaryMaskedRay");
 
         sprite = GetNode<AnimatedSprite2D>("./AnimatedSprite2D");
         sprite.Play();
@@ -38,9 +38,9 @@ public partial class Player : Area2D, IMover {
     }
 
     private void Move(Vector2 direction, double delta) {
-        rayCast.TargetPosition = direction * 10;
-        rayCast.ForceRaycastUpdate();
-        if (rayCast.IsColliding()) {
+        boundaryMaskedRay.TargetPosition = direction * 10;
+        boundaryMaskedRay.ForceRaycastUpdate();
+        if (boundaryMaskedRay.IsColliding()) {
             return;
         }
         Position = Position + direction * speed * (float)delta;
